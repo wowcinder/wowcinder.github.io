3bbaaa414ad949d7b20c30a61facce9894d4b50f
xuehuihe <xuehuihe.java@gmail.com>
2013-08-04 15:27:55 +0800

    memo add

---
+++ _posts/memo/2013-08-04-memo.mk
@@ -0,0 +1,23 @@
+---
+layout: post
+category : memo
+---
+{% include JB/setup %}
+
+#google dns
+	8.8.8.8
+	8.8.4.4
+#appspot
+	74.125.128.141
+	#hexh2003-hrd.appspot.com
+#google cdn
+	203.208.46.146 dl.google.com
+	203.208.46.146 dl-ssl.google.com
+
+
+
+<!--more-->
+
+
+
+
